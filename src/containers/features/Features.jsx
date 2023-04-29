c2c8fd7e451b0ce6e9570608ae592b8e53c4eb1a
aasmigill <gill.aasmi10@gmail.com>
2023-04-29 13:17:41 +0530

    updated festeria

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,13 +1,84 @@
-import React from 'react';
-// import Feature from '../../components/feature/Feature';
+import React, { useState } from 'react';
 import './features.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-const Features = () => (
-  <div className="gpt3__features section__padding" id="features">
-    <div className="gpt3__features-heading">
-      <h1 className="gradient__text">Calender</h1>
+function Features() {
+  const [date, setDate] = useState(new Date());
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">Calendar</h1>
+      </div>
+      <div className="app">
+        <h1 className="text-center">React Calendar</h1>
+        <div className="calendar-container">
+          <Calendar onChange={setDate} value={date} />
+        </div>
+        <p className="text-center">
+          <span className="bold">Selected Date:</span>{' '}
+          {date.toDateString()}
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Features;
+// import Fullcalendar from '@fullcalendar/react';
+// import dayGridPlugin from '@fullcalendar/daygrid';
+// import timeGridPlugin from '@fullcalendar/timegrid';
+// import interactionPlugin from '@fullcalendar/interaction';
+
+// const Features = () => (
+//   <div className="gpt3__features section__padding" id="features">
+//     <div className="gpt3__features-heading">
+//       <h1 className="gradient__text">Calender</h1>
+//       <br />
+//       <br />
+//     </div>
+//     <div>
+//       <Fullcalendar
+//         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+//         initialView="dayGridMonth"
+//         headerToolbar={{
+//           start: 'today prev,next', // will normally be on the left. if RTL, will be on the right
+//           center: 'title',
+//           end: 'dayGridMonth,timeGridWeek,timeGridDay', // will normally be on the right. if RTL, will be on the left
+//           // title: "titleOptions"
+//         }}
+//         // titleFormat={{ color: 'white' }}
+//         height="90vh"
+//         aspectRatio="1:1"
+//       />
+//     </div>
+//   </div>
+// );
+
+// export default Features;
+// const features = () => (
+
+// )
+// const features = () => (
+//   <div className="gpt3__features section__padding" id="features">
+//    <div className="gpt3__features-heading">
+//       <h1 className="gradient__text">Calender</h1>
+//       <br />
+//       <br />
+//     </div>
+//     </div>
+//     const [date, setDate] = useState(new Date());
+//     <div className='app'>
+//       <h1 className='text-center'>React Calendar</h1>
+//       <div className='calendar-container'>
+//         <Calendar onChange={setDate} value={date} />
+//       </div>
+//       <p className='text-center'>
+//         <span className='bold'>Selected Date:</span>{' '}
+//         {date.toDateString()}
+//       </p>
+//     </div>
+
+// )
+
+// export default Features;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,17 +1,41 @@
+// import React from 'react';
+// // import possibilityImage from '../../assets/possibility.png';
+// import './possibility.css';
+
+// const Possibility = () => (
+//   <div className="Upcoming_events" id="possibility">
+//     <h1 className="Upcoming_events_title">Upcoming Events</h1>
+//     <div className="cards">
+//       <div className="row">
+//         <div className="card">Code for a Cause</div>
+//         <div className="card">Hack the Hackathon</div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+// export default Possibility;
 import React from 'react';
-// import possibilityImage from '../../assets/possibility.png';
 import './possibility.css';
 
 const Possibility = () => (
   <div className="Upcoming_events" id="possibility">
     <h1 className="Upcoming_events_title">Upcoming Events</h1>
     <div className="cards">
-      <div className="card">Code for a Cause
-        <img src="./hack3.png" />
+      <div className="row">
+        <div className="card">Code for a Cause</div>
+        <div className="card">Hack the Hackathon</div>
+        <div className="card">BitBox 3.0</div>
+        <div className="card">Codevlabs</div>
       </div>
-      <div className="card">Hack the Hackathon</div>
+      {/* <div className="row">
+        <div className="card">Event 4</div>
+        <div className="card">Event 5</div>
+        <div className="card">Event 6</div>
+      </div> */}
     </div>
   </div>
 );
 
 export default Possibility;
+
